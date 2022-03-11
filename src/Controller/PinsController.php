02440162eba704958801ec775ee226d7b37cd91c
Eric Paoli <ericpaoli65@gmail.com>
2022-03-11 10:34:33 +0100

    add show page and navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,5 +18,13 @@ class PinsController extends AbstractController
 
 
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
