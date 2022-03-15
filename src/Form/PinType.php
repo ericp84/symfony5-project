e1797f68e5848f1cf80a582fcbe8d04ba5b7f5fc
Eric Paoli <ericpaoli65@gmail.com>
2022-03-15 13:57:46 +0100

    image upload fix

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -7,12 +7,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (fichier JPG ou PNG)',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'supprimer',
+                'download_uri' => false,
+                'image_uri' => true,
+            ])
             ->add('title')
             ->add('description', TextareaType::class)
         ;
