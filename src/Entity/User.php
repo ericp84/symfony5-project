2a56aa00a47dc1dd190d9f829c1b0078519ebb1e
Eric Paoli <ericpaoli65@gmail.com>
2022-03-16 22:10:29 +0100

    display pin owner and created method getFullName to get user's full name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -204,4 +204,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): string 
+    {
+        return $this -> getFirstName() . ' ' . $this -> getLastName();
+    }
 }
