8868996a596f796f7af960902f4bab56955ee73c
Eric Paoli <ericpaoli65@gmail.com>
2022-03-17 11:06:08 +0100

    style modify

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -28,13 +29,15 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/create", name="app_pins_create", methods="GET|POST")
      */
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo ): Response
     {
         $pin = new  Pin;
         $form = $this->createForm(PinType::class, $pin);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $janeDoe = $userRepo -> findOneBy(['email' => 'janedoe@example.com']);
+            $pin -> setUser($janeDoe);
             $em -> persist($pin);
             $em -> flush();
 
@@ -58,7 +61,7 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "PUT", "POST"})
      */
-    public function edit(Request $request, Pin $pin, EntityManagerInterface $em ): Response
+    public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(PinType::class, $pin);
 
