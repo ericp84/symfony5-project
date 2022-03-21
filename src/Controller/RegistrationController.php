c7cebe883a4c890037b93bd083b0cad90e278e77
Eric Paoli <ericpaoli65@gmail.com>
2022-03-21 12:13:56 +0100

    use container parameters to extract sender details for mailing

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -56,7 +56,10 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@pintest.com', 'pintest'))
+                    ->from(new Address(
+                        $this->getParameter('app.mail_from_address'),
+                        $this->getParameter('app.mail_from_name'),  
+                    ))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -19,6 +19,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 class Pin
 {
     use Timestampable;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
