09ff210bd59f389ae2d9701cdbb290c8a90f4cc7
Eric Paoli <ericpaoli65@gmail.com>
2022-03-22 12:25:33 +0100

    add profile page and styling

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/account", name="app_account")
+     */
+    public function show(): Response
+    {
+        return $this->render('account/show.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
